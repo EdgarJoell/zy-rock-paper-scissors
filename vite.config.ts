d71f1214ceb598f12c60f73d8afff9ee11389986
Edgar Zambrana <edgarjoel0920@gmail.com>
2025-11-02 01:38:22 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/zy-rock-paper-scissors/',
+    base: 'zy-rock-paper-scissors',
     plugins: [react()],
 })
